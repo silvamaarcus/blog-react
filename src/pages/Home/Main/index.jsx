ad82f3978d465019031b340f7ed9939e01ccec6a
Marcus Silva <devsilvamarcus@gmail.com>
2023-10-23 15:01:35 -0300

    Update: Requisições na API com resultado na interface

--- src/pages/Home/Main/index.jsx
+++ src/pages/Home/Main/index.jsx
@@ -1,30 +1,41 @@
+// Hooks
+import { useState, useEffect } from "react";
+// API
+import api from "../../../services/api";
 
+const Main = ({ content }) => {
+  const [user, setUser] = useState([]);
 
-const Main = () => {
+  useEffect(() => {
+    // Executar o código apenas se tiver algo dentro de content.
+    if (content) {
+      api.get("/user/" + content.id_user).then((r) => {
+        setUser(r.data);
+      });
+    }
+  }, []);
+
   return (
     <>
       <div className="bb-black py-4">
         <div>
-          <h6 className="uppercase color-gray">01 nov 2021</h6>
-          <h6 className="uppercase color-primary">tecnologia</h6>
-          <h5 className="mt-3">O que tem de novo no PS5? Muitas novidades?</h5>
-          <p className="mt-1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare urna
-            pharetra ut ac, pellentesque.{" "}
-          </p>
+          <h6 className="uppercase color-gray">{content.date}</h6>
+          <h6 className="uppercase color-primary">{content.category}</h6>
+          <h5 className="mt-3">{content.title}</h5>
+          <p className="mt-1">{content.resume}</p>
         </div>
 
         <div className="flex mt-3">
           <div>
             <img
-              src="profile/alex.png"
+              src={user.ImageProfile}
               className="profile profile-img"
               alt=""
             />
           </div>
           <div className="ml-2">
-            <h6 className="color-primary">Bruce Wayne</h6>
-            <h6 className="color-gray h7">Autor</h6>
+            <h6 className="color-primary">{user.name + " " + user.surname}</h6>
+            <h6 className="color-gray h7">{"@" + user.user} </h6>
           </div>
         </div>
       </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,11 +7,54 @@ import Main from "./Main";
 // Figuras
 import star from "../../svg/icon-star.svg";
 
+//API
+import api from "../../services/api";
+
+//Hooks
+import { useState, useEffect } from "react";
+
 const Home = () => {
+  // Variáveis de estado:
+
+  // Section Main
+  // main -> variável de estado.
+  // setMain -> função que vai atualizar esse estado.
+  const [main, setMain] = useState([]);
+  // Section More Views
+  const [mostseen, setMostseen] = useState([]);
+  // Section Banner
+  const [banner, setBanner] = useState([]);
+
+  //Requisições:
+
+  // Faça isso, quando executar o componente.
+  useEffect(() => {
+    // Puxando informações dos "posts" c/ metodo get()
+    // Aplicando filtros com parametros:
+    // posts -> postagens do JSON.
+    // star=5 -> postagens com 5 estrelas de nota.
+    // _limit=2 -> limitar busca por 2 postagens apenas.
+    // order=desc -> listar em ordem descrescente.
+    // _sort=date -> listar por data de criação.
+
+    // Requisição para listar posts com nota (star) igual a 5.
+    api.get("/posts?star=5&_limit=2&order=desc").then((r) => {
+      setMain(r.data);
+    });
+    // Requisição para listar o último banner adicionado.
+    api.get("/posts?_sort=date&order=desc&_limit=1").then((r) => {
+      setBanner(r.data);
+    });
+    // Requisição para listar 3 posts.
+    api.get("/posts?_limit=3").then((r) => {
+      setMostseen(r.data);
+    });
+  }, []);
+
   return (
     <>
       <Hero />
-      
+
       <main className="container">
         <div className="row">
           <div className="grid-6 pt-5 pb-3">
@@ -26,18 +69,17 @@ const Home = () => {
           </div>
 
           <div className="grid-6">
-            <Main />
-            <Main />
+            {main.map((item) => {
+              return <Main key={item.id} content={item} />;
+            })}
           </div>
         </div>
       </main>
 
-      <div class="bg-section">
+      <div class="bg-section"> 
         <section class="container">
           <h3 class="ml-2 mb-3">Mais vistos</h3>
           <div class="row">
-            <Card />
-            <Card />
             <Card />
           </div>
         </section>
